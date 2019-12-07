581af4048f1ea1c4f381fad642d3805bc7af8edc
mohammad esmaiel Emadi <mohesem@gmail.com>
2019-12-07 11:59:20 +0330

    7 sebtember

--- index.js
+++ index.js
@@ -32,55 +32,55 @@ mongoose.connect("mongodb://emad:mohesem1368@localhost:27017/test", {
 mongoose.connection.on("connected", () => {
   console.log("MongoDB connected");
 
-  fs.createReadStream(`./layers/1.geojson`).pipe(
-    //   /* COUNTRIES */
-    //   geojsonStream.parse((country, index) => {
-    //     GeoModelBbox.findOne({ name0: country.properties.NAME_0 }, (err, doc) => {
-    //       if (err) return err;
-    //       if (doc) {
-    //         console.log("doc exist on db ");
-    //       }
-    //       if (!doc) {
-    //         const newCoord = turf.cleanCoords(country).geometry.coordinates;
-    //         const multiPoly = turf.multiPolygon(newCoord, {
-    //           name: country.properties.NAME_0
-    //         });
-    //         const bbox = turf.bbox(multiPoly);
-    //         const bboxPolygon = turf.bboxPolygon(bbox);
-    //         const newGeoModelBbox = new GeoModelBbox({
-    //           name0: multiPoly.properties.name,
-    //           geo: bboxPolygon.geometry
-    //         });
+  fs.createReadStream(`./layers/2.geojson`).pipe(
+    // /* COUNTRIES */
+    // geojsonStream.parse((country, index) => {
+    //   GeoModelBbox.findOne({$and: [
+    //             { name0: country.properties.NAME_0 },
+    //             { name1: country.properties.NAME_1 }
+    //           ]}, (err, doc) => {
+    //     if (err) return err;
+    //     if (doc) {
+    //       console.log("doc exist on db ");
+    //     }
+    //     if (!doc) {
+    //       const newCoord = turf.cleanCoords(country).geometry.coordinates;
+    //       const multiPoly = turf.multiPolygon(newCoord, {
+    //         name: country.properties.NAME_0
+    //       });
+    //       const bbox = turf.bbox(multiPoly);
+    //       const bboxPolygon = turf.bboxPolygon(bbox);
+    //       const newGeoModelBbox = new GeoModelBbox({
+    //         name0: multiPoly.properties.name,
+    //         geo: bboxPolygon.geometry
+    //       });
 
-    //         const newGeoModel = new GeoModel({
-    //           name0: multiPoly.properties.name,
-    //           geo: multiPoly.geometry
-    //         });
+    //       const newGeoModel = new GeoModel({
+    //         name0: multiPoly.properties.name,
+    //         geo: multiPoly.geometry
+    //       });
 
-    //         newGeoModelBbox.save(err => {
-    //           if (err) console.log(err);
-    //           console.log(`${country.properties.NAME_0} bbox saved`);
-    //         });
+    //       newGeoModelBbox.save(err => {
+    //         if (err) console.log(err);
+    //         console.log(`${country.properties.NAME_0} bbox saved`);
+    //       });
 
-    //         newGeoModel.save(err => {
-    //           if (err) {
-    //             const options = { tolerance: 0.003, highQuality: true };
-    //             const simplified = turf.simplify(multiPoly, options);
-    //             const simplifiedModel = new GeoModel({
-    //               name0: multiPoly.properties.name,
-    //               geo: simplified.geometry
-    //             });
-    //             simplifiedModel.save();
-    //           }
-    //           console.log(`${country.properties.NAME_0} polygon saved`);
-    //         });
-    //       }
-    //     });
-    //   })
+    //       newGeoModel.save(err => {
+    //         if (err) {
+    //           const options = { tolerance: 0.003, highQuality: true };
+    //           const simplified = turf.simplify(multiPoly, options);
+    //           const simplifiedModel = new GeoModel({
+    //             name0: multiPoly.properties.name,
+    //             geo: simplified.geometry
+    //           });
+    //           simplifiedModel.save();
+    //         }
+    //         console.log(`${country.properties.NAME_0} polygon saved`);
+    //       });
+    //     }
+    //   });
+    // })
 
-
-
-    
     /* LEVEL_1 */
     // geojsonStream.parse((level_1, index) => {
     //   GeoModelBbox.findOne(
@@ -136,6 +136,70 @@ mongoose.connection.on("connected", () => {
     //     }
     //   );
     // })
+
+    /* LEVEL_2 */
+    geojsonStream.parse((level_2, index) => {
+      GeoModelBbox.findOne(
+        {
+          $and: [
+            { name0: level_2.properties.NAME_0 },
+            { name1: level_2.properties.NAME_1 },
+            { name2: level_2.properties.NAME_2 }
+          ]
+        },
+        (err, doc) => {
+          if (err) return err;
+          if (doc) {
+            console.log("doc exist on db ");
+          }
+          if (!doc) {
+            const newCoord = turf.cleanCoords(level_2).geometry.coordinates;
+            const multiPoly = turf.multiPolygon(newCoord, {
+              name: level_2.properties.NAME_2
+            });
+            const bbox = turf.bbox(multiPoly);
+            const bboxPolygon = turf.bboxPolygon(bbox);
+            const newGeoModelBbox = new GeoModelBbox({
+              name0: level_2.properties.NAME_0,
+              name1: level_2.properties.NAME_1,
+              name1: level_2.properties.NAME_2,
+              geo: bboxPolygon.geometry
+            });
+
+            const newGeoModel = new GeoModel({
+              name0: level_2.properties.NAME_0,
+              name1: level_2.properties.NAME_1,
+              name1: level_2.properties.NAME_2,
+              geo: multiPoly.geometry
+            });
+
+            newGeoModelBbox.save(err => {
+              if (err) console.log(err);
+              console.log(
+                `${level_2.properties.NAME_2} in ${level_2.properties.NAME_1} in ${level_2.properties.NAME_0} bbox saved`
+              );
+            });
+
+            newGeoModel.save(err => {
+              if (err) {
+                const options = { tolerance: 0.003, highQuality: true };
+                const simplified = turf.simplify(multiPoly, options);
+                const simplifiedModel = new GeoModel({
+                  name0: level_2.properties.NAME_0,
+                  name1: level_2.properties.NAME_1,
+                  name1: level_2.properties.NAME_2,
+                  geo: simplified.geometry
+                });
+                simplifiedModel.save();
+              }
+              console.log(
+                `${level_2.properties.NAME_2} in ${level_2.properties.NAME_1} in ${level_2.properties.NAME_0} bbox saved`
+              );
+            });
+          }
+        }
+      );
+    })
   );
 });
 
